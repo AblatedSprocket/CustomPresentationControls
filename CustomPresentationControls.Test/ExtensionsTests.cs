3ffc4bae1149e910b0f01111ab64fe169384d46a
unknown <andrewwburch@gmail.com>
2018-08-24 11:03:42 -0500

    Added Extensions for retrieving parent element information

---
+++ CustomPresentationControls.Test/ExtensionsTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using CustomPresentationControls.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CustomPresentationControls.Test
+{
+    [TestClass]
+    public class ExtensionsTests
+    {
+        [TestMethod]
+        public void FindNearestAncestorTag_WhenValidTagAvailable_ReturnsTag()
+        {
+            //ARRANGE:
+            StackPanel panel = new StackPanel();
+            panel.Tag = true;
+            Border border = new Border();
+            Button button = new Button();
+            //ACT:
+            border.Child = button;
+            panel.Children.Add(border);
+            var tag = button.FindNearestAncestorTag();
+            //ASSERT:
+            Assert.IsTrue((bool)tag);
+        }
+        [TestMethod]
+        public void FindNthNearestAncestorTag_WhenNIsTwo_ReturnsSecondTagEncounteredInHierarchy()
+        {
+            //ARRANGE:
+            StackPanel panel = new StackPanel();
+            panel.Tag = true;
+            Border border = new Border();
+            border.Tag = false;
+            Button button = new Button();
+            //ACT:
+            border.Child = button;
+            panel.Children.Add(border);
+            var tag = button.FindNthNearestAncestorTag(2);
+            //ASSERT:
+            Assert.IsTrue((bool)tag);
+
+        }
+        [TestMethod]
+        public void FindParent_WhenNoParentCanBeFound_ReturnsNull()
+        {
+            //ARRANGE:
+            StackPanel panel = new StackPanel();
+            Button button = new Button();
+            Border border = new Border();
+            //ACT:
+            border.Child = button;
+            panel.Children.Add(border);
+            var parent = button.FindParent<Grid>();
+            //ASSERT:
+            Assert.IsNull(parent);
+        }
+        [TestMethod]
+        public void FindParent_WhenValidParentRequested_ReturnsType()
+        {
+            //ARRANGE:
+            StackPanel panel = new StackPanel();
+            Button button = new Button();
+            Border border = new Border();
+            //ACT:
+            border.Child = button;
+            panel.Children.Add(border);
+            var parent = button.FindParent<StackPanel>();
+            //ASSERT:
+            Assert.IsNotNull(parent);
+
+        }
+    }
+}
---
+++ CustomPresentationControls/Utilities/Extensions.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CustomPresentationControls.Utilities
+{
+    public static class Extensions
+    {
+        public static object FindNearestAncestorTag(this FrameworkElement child)
+        {
+            if (child != null)
+            {
+                FrameworkElement parent = VisualTreeHelper.GetParent(child) as FrameworkElement;
+                if (parent.Tag != null)
+                {
+                    return parent.Tag;
+                }
+                else
+                {
+                    return FindNearestAncestorTag(parent);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public static object FindNthNearestAncestorTag(this FrameworkElement child, int n)
+        {
+            int tagCount = 0;
+            if (child != null)
+            {
+                FrameworkElement parent = null;
+                do
+                {
+                    parent = VisualTreeHelper.GetParent(child) as FrameworkElement;
+                    if (parent != null && parent.Tag != null)
+                    {
+                        if (++tagCount == n)
+                        {
+                            return parent.Tag;
+                        }
+                        child = parent;
+                    }
+                } while (parent != null);
+            }
+            return null;
+        }
+        public static T FindParent<T>(this DependencyObject child) where T : DependencyObject
+        {
+            if (child != null)
+            {
+                DependencyObject parent = VisualTreeHelper.GetParent(child);
+                if (parent is T)
+                {
+                    return parent as T;
+                }
+                else
+                {
+                    return FindParent<T>(parent);
+                }
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
